f0b21f890714f5ee6fd7c5d33374c3a83b255ed0
readf0x <davis.a.forsythe@gmail.com>
2026-02-17 07:45:02 -0600

    fix treesitter

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -2,10 +2,10 @@ return require"genvim".inject {
   { name = "nvim-treesitter", lazy = false,
     dependencies = {"nvim-treesitter-context"},
     config = function()
-      require"nvim-treesitter.config".setup {
+      require"nvim-treesitter".setup {
+        install_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "site"),
         highlight = { enable = true },
         indent = { enable = true },
-        parser_install_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "site"),
       }
     end,
   },
